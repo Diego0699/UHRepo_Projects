4afe5d1188863b96cff29a98f35fb143567211db
DESKTOP-BEGGONK\juand <juan.d99@hotmail.com>
2022-10-13 23:08:20 -0600

    SomeChanges

--- UHProject/src/app/productos/producto-list/producto-list.component.ts
+++ UHProject/src/app/productos/producto-list/producto-list.component.ts
@@ -12,25 +12,25 @@ export class ProductoListComponent implements OnInit {
   
 
 
-  products:Product[];
-  products2:Product[];
-  products3:Product[];
-  products4:Product[];
-  products5:Product[];
-  products6:Product[];
-  products7:Product[];
+  Placas:Product[];
+  Procesadores:Product[];
+  Ram:Product[];
+  Almacenamiento:Product[];
+  Enfriamiento:Product[];
+  Fuentes:Product[];
+  Graficas:Product[];
 
   constructor(private productService:ProductService,
               private router:Router) { }
 
   ngOnInit() {
-    this.products = this.productService.getProducts();
-    this.products2 = this.productService.getProducts2();
-    this.products3 = this.productService.getProducts3();
-    this.products4 = this.productService.getProducts4();
-    this.products5 = this.productService.getProducts5();
-    this.products6 = this.productService.getProducts6();
-    this.products7 = this.productService.getProducts7();
+    this.Placas = this.productService.getProducts();
+    this.Procesadores = this.productService.getProducts2();
+    this.Ram = this.productService.getProducts3();
+    this.Almacenamiento = this.productService.getProducts4();
+    this.Enfriamiento = this.productService.getProducts5();
+    this.Fuentes = this.productService.getProducts6();
+    this.Graficas = this.productService.getProducts7();
   }
   
 
